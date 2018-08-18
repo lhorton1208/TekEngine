6fe6329cc9ceb331fa7f8e6f908ce5b34f8ccb50
lhorton1208 <larryeh1208@gmail.com>
2018-08-17 22:39:52 -0400

    Add files via upload
    second batch of files comprising the solution

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TekEngine2018.Startup))]
+namespace TekEngine2018
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
