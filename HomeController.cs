110a6ca08651eaf11356c47959494a1db821780f
lhorton1208 <larryeh1208@gmail.com>
2018-08-20 20:53:39 -0400

    Changes from August 18th

---
+++ HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TekEngine2018.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+        public ActionResult TechnicalConsulting()
+        {
+            return View("TechnicalConsulting");
+        }
+        public ActionResult ManagerialConsulting()
+        {
+            return View("ManagerialConsulting");
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
